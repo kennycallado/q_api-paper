27d46ec3708896b8888fb4d3e36184ad8c68e3ca
kennycallado <kennycallado@hotmail.com>
2023-05-29 17:14:11 +0200

    ADD Agrega migraciones para paper_answers

---
+++ src/database/migrations/2023-05-29-145829_create_paper_answers/up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS paper_answers ();
+
+ALTER TABLE paper_answers
+  ADD COLUMN id SERIAL PRIMARY KEY,
+  ADD COLUMN paper_id SERIAL NOT NULL,
+  ADD COLUMN answer_id SERIAL NOT NULL,
+  ADD CONSTRAINT fk_pa_paper_id FOREIGN KEY (paper_id) REFERENCES papers (id) ON DELETE CASCADE;
+
+INSERT INTO paper_answers (paper_id, answer_id) VALUES
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (2, 4),
+  (2, 5)
+  ;
--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,6 +1,14 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    paper_answers (id) {
+        id -> Int4,
+        paper_id -> Int4,
+        answer_id -> Int4,
+    }
+}
+
+diesel::table! {
     papers (id) {
         id -> Int4,
         user_id -> Int4,
@@ -9,3 +17,10 @@ diesel::table! {
         completed -> Bool,
     }
 }
+
+diesel::joinable!(paper_answers -> papers (paper_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    paper_answers,
+    papers,
+);
