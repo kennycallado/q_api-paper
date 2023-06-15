98392a9e471d6ac56ba83ad1504f6d37cddb44cc
kennycallado <kennycallado@hotmail.com>
2023-06-15 16:20:03 +0200

    ADD ∙ Agrega record y agrega funcionalidad a project

--- src/app/providers/models/project.rs
+++ src/app/providers/models/project.rs
@@ -4,9 +4,11 @@ use rocket::{http::Status, State};
 use serde::{Deserialize, Serialize};
 
 use crate::app::providers::config_getter::ConfigGetter;
+use crate::app::providers::models::record::{PubRecord, PubNewRecord};
 
 #[cfg(feature = "fetch")]
 use crate::app::providers::services::fetch::Fetch;
+
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -18,11 +20,7 @@ pub struct PubProject {
 
 #[cfg(feature = "fetch")]
 impl PubProject {
-    pub async fn init_user(
-        fetch: &State<Fetch>,
-        project_id: i32,
-        user_id: i32,
-    ) -> Result<Self, Status> {
+    pub async fn init_user(fetch: &State<Fetch>, project_id: i32, user_id: i32) -> Result<Self, Status> {
         let robot_token = match Fetch::robot_token().await {
             Ok(token) => token,
             Err(_) => return Err(Status::InternalServerError),
@@ -35,13 +33,17 @@ impl PubProject {
             + user_id.to_string().as_str()
             + "/new";
 
-        let client = fetch.client.lock().await;
-        let res = client
-            .get(project_url)
-            .header("Accept", "application/json")
-            .header("Authorization", robot_token)
-            .send()
-            .await;
+        let res;
+        {
+            let client = fetch.client.lock().await;
+            res = client
+                .get(project_url)
+                .header("Accept", "application/json")
+                .header("Authorization", robot_token)
+                .header("Content-Type", "application/json")
+                .send()
+                .await;
+        }
 
         match res {
             Ok(res) => {
@@ -50,6 +52,42 @@ impl PubProject {
                 }
 
                 Ok(res.json::<Self>().await.unwrap())
+            }
+            Err(_) => Err(Status::InternalServerError),
+        }
+    }
+
+    pub async fn store_record(fetch: &State<Fetch>, project_id: i32, new_record: PubNewRecord) -> Result<PubRecord, Status> {
+        let robot_token = match Fetch::robot_token().await {
+            Ok(token) => token,
+            Err(_) => return Err(Status::InternalServerError),
+        };
+
+        let project_url = ConfigGetter::get_entity_url("project")
+            .unwrap_or("http://localhost:8051/api/v1/project/".to_string())
+            + project_id.to_string().as_str()
+            + "/record";
+
+        let res;
+        {
+            let client = fetch.client.lock().await;
+            res = client
+                .post(project_url)
+                .header("Accept", "application/json")
+                .header("Authorization", robot_token)
+                .header("Content-Type", "application/json")
+                .json(&new_record)
+                .send()
+                .await;
+        }
+
+        match res {
+            Ok(res) => {
+                if !res.status().is_success() {
+                    return Err(Status::from_code(res.status().as_u16()).unwrap());
+                }
+
+                Ok(res.json::<PubRecord>().await.unwrap())
             }
             Err(_) => Err(Status::InternalServerError),
         }
