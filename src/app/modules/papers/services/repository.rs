e56fb51fe429265df724b69d758058081b01d904
kennycallado <kennycallado@hotmail.com>
2023-07-18 10:26:31 +0200

    FIX Returning correct lasts papers of a given project

--- src/app/modules/papers/services/repository.rs
+++ src/app/modules/papers/services/repository.rs
@@ -69,12 +69,12 @@ pub async fn patch_completed(db: &Db, id: i32) -> Result<usize, diesel::result::
 
 pub async fn last_paper_for_all_users_where_project_id(db: &Db, project_id: i32) -> Result<Vec<Paper>, diesel::result::Error> {
     let papers = db.run(move |conn| {
+        // .filter(papers::completed.eq(true))
         papers::table
             .select(papers::all_columns)
             .distinct_on(papers::user_id)
             .filter(papers::project_id.eq(project_id))
-            // .filter(papers::completed.eq(true))
-            .order(papers::user_id.desc())
+            .order((papers::user_id, papers::id.desc()))
             .load::<Paper>(conn)
     }).await;
 
